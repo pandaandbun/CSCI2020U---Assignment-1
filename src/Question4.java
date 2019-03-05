9c3d104e46aa8f8c0e7ee5852b99074a56bd4786
katanaWieldingPanda <Rico0Sandard@gmail.com>
2019-03-04 22:17:26 -0500

    Add Q4

--- src/Question4.java
+++ src/Question4.java
@@ -1,37 +1,86 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import java.util.Random;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Insets;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Question4 extends Application {
+	static int[] counts;
+
 	@Override
-	public void start (Stage primaryStage) {
+	public void start(Stage primaryStage) {
+		HBox controlBox = new HBox(10);
+		BorderPane pane = new BorderPane();
+		Pane chart = new Pane();
 		
-		Random rn = new Random();
-		
-		GridPane pane = new GridPane();
-	    pane.setAlignment(Pos.CENTER);
-	    pane.setHgap(5);
-	    
-	    for (int i = 0; i < 3; i++) {
-	    	int cardChoosen = rn.nextInt(54) + 1;
-	    	String cardLocation = "Cards/" + cardChoosen + ".png";
-	    	ImageView imageView1 = new ImageView(cardLocation);
-	    	pane.add(imageView1, i, 0);
-	    }
-	    
-	    // Create a scene and place it in the stage
-	    Scene scene = new Scene(pane);
-	    primaryStage.setTitle("Question 1"); // Set the stage title
-	    primaryStage.setScene(scene); // Place the scene in the stage
-	    primaryStage.show(); // Display the stage
+		Histogram hist = new Histogram();
+		// Set text field
+		TextField textFilename = new TextField();
+		// Set button
+		Button buttonView = new Button("View");
+
+		// Set text field to listen for ENTER
+		textFilename.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				readFile(textFilename,hist);
+			}
+		});
+		// Set action on button pressed
+		buttonView.setOnAction(e -> readFile(textFilename,hist));
+		// Set control menu
+		controlBox.getChildren().addAll(new Label("Filename"), textFilename, buttonView);
+		// Set root
+		pane.setCenter(hist);
+		pane.setBottom(controlBox);
+		pane.setMargin(hist, new Insets(20));
+
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane);
+		primaryStage.setTitle("Question 4"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+
+	private void readFile(TextField textFilename, Histogram hist) {
+		counts = new int[26];
+		File file = new File(textFilename.getText());
+
+		try (Scanner input = new Scanner(file);) {
+			String fileString = "";
+			while (input.hasNext()) {
+				fileString += input.nextLine().toLowerCase() + "\n";
+			}
+
+			countLetters(fileString);
+			hist.setCounts(counts);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+	public static void countLetters(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			char character = s.charAt(i);
+
+			if (Character.isLetter(character)) {
+				counts[character - 97]++;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		launch (args);
+		launch(args);
 	}
-
 }
